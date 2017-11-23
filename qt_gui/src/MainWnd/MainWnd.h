f1581359c1d7317cce62079916f114a8818fea5f
DaisukeAtaraxiA <d.morikawa.b@gmail.com>
2017-11-23 23:42:40 +0900

    空のメインウィンドウを実装

---
+++ qt_gui/src/MainWnd/MainWnd.h
@@ -0,0 +1,30 @@
+﻿//----------------------------------------------------------------------------
+// MainWnd.h : メインウィンドウの作成・管理を行う
+//----------------------------------------------------------------------------
+#ifndef MainWndH
+#define MainWndH
+
+class CApp;
+#include <QMainWindow>
+#include "../Common/Define.h"
+#include "ui_MainWnd.h"
+//----------------------------------------------------------------------------
+// メインウィンドウの作成・管理を行うクラス
+//----------------------------------------------------------------------------
+class CMainWnd : public QMainWindow, protected Ui::MainWnd
+{
+public: // 関数
+
+	CMainWnd(CApp & app): m_rApp(app) { }
+
+	virtual BOOL Create() { return OnCreate(); }
+
+	virtual LRESULT OnCreate();
+
+protected: // メンバ変数
+
+	CApp & m_rApp;
+};
+//----------------------------------------------------------------------------
+
+#endif
