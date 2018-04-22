156170260df073847a72a7e877a0022b364c79da
DaisukeAtaraxiA <d.morikawa.b@gmail.com>
2018-04-22 15:15:58 +0900

    日本語ローカライゼーション

--- qt_gui/src/TimerPlayWnd/TimerPlayWnd_MainWnd.cpp
+++ qt_gui/src/TimerPlayWnd/TimerPlayWnd_MainWnd.cpp
@@ -27,6 +27,13 @@ CTimerPlayWnd_MainWnd::CTimerPlayWnd_MainWnd(CMainWnd & mainWnd)
 	m_edit4->setValue(localNow->tm_min);
 	m_edit4->setRange(0, 59);
 
+	if (QLocale::system().name() == "ja_JP") {
+		m_label3->setText(m_label->text());
+		m_label->setText(QString());
+		m_label6->setText(m_label4->text());
+		m_label4->setText(QString());
+	}
+
 	EnableControl(true, false);
 
 	connect(m_playRadioButton, &QRadioButton::clicked,
@@ -45,7 +52,7 @@ void CTimerPlayWnd_MainWnd::EnableControl(bool bPlayEnable, bool bStopEnable)
 {
 	m_label->setEnabled(bPlayEnable);
 	m_label2->setEnabled(bPlayEnable);
-	m_label3->setEnabled(bStopEnable);
+	m_label3->setEnabled(bPlayEnable);
 	m_edit->setEnabled(bPlayEnable);
 	m_edit2->setEnabled(bPlayEnable);
 
