717884804aff499615ef3399d3cabf63786c8d38
sugartarou <sugartarou1206@gmail.com>
2024-03-02 23:19:25 +0900

    getlibs.batの修正#488
    - 遅延環境変数の対応を追加

--- getlibs.bat
+++ getlibs.bat
@@ -1,20 +1,22 @@
 @echo off
+SETLOCAL enabledelayedexpansion
+
 rem ˆê•”‚Ìƒ‰ƒCƒuƒ‰ƒŠ‚Ìƒrƒ‹ƒh‚Ég‚¤Visual Studio“à•”‚Ìbat‚ğ’Tõ
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" (
     SET VSDEVCMD="%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-    echo %VSDEVCMD% ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
+    echo !VSDEVCMD! ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
     echo Visual Studio Community 2022 ‚ğ—˜—p‚µ‚Ü‚·
 ) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" (
     SET VSDEVCMD="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-    echo %VSDEVCMD% ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
+    echo !VSDEVCMD! ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
     echo Visual Studio Community 2017 ‚ğ—˜—p‚µ‚Ü‚·
 ) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" (
     SET VSDEVCMD="%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
-    echo %VSDEVCMD% ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
+    echo !VSDEVCMD! ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
     echo Visual Studio Enterprise 2022 ‚ğ—˜—p‚µ‚Ü‚·
 ) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" (
     SET VSDEVCMD="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-    echo %VSDEVCMD% ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
+    echo !VSDEVCMD! ‚ªŒ©‚Â‚©‚è‚Ü‚µ‚½
     echo Visual Studio Enterprise 2017 ‚ğ—˜—p‚µ‚Ü‚·
 ) else (
     echo ƒCƒ“ƒXƒg[ƒ‹Ï‚İ‚ÌVisual Studio‚à‚µ‚­‚Í‚»‚Ì“à•”‚ÌVsDevCmd.bat‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ‚Å‚µ‚½
@@ -26,8 +28,8 @@ if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Community\Common7\Too
     echo "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
     echo "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
     SET /P VSDEVCMD="VsDevCmd.bat ‚ÌƒpƒX‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢->"
-    if not exist "%VSDEVCMD%" (
-        echo "%VSDEVCMD%"
+    if not exist "!VSDEVCMD!" (
+        echo "!VSDEVCMD!"
         echo [91m“ü—Í‚³‚ê‚½ƒpƒX‚ÉVsDevCmd.bat‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ[37m
         goto ERROR_VSDEVCMD_NOT_FOUND
     )
@@ -46,7 +48,7 @@ rem ƒ‰ƒCƒuƒ‰ƒŠ‚Ìƒ_ƒEƒ“ƒ[ƒh‚Æzip“WŠJ
 echo Às’†‚ÍPowerShell‚Ì‰æ–Ê‚ª•\¦‚³‚ê‚Ü‚·
 echo ‰æ–Ê‚É‚ÍG‚ç‚¸‚»‚Ì‚Ü‚Ü‚Å‚¨‘Ò‚¿‚­‚¾‚³‚¢
 powershell -ExecutionPolicy Unrestricted ./downloadlibs.ps1
-if %ERRORLEVEL% equ 1 (
+if !ERRORLEVEL! equ 1 (
     goto ERROR_FAILED_TO_GET_LIBS
 )
 
@@ -55,8 +57,8 @@ echo ƒrƒ‹ƒh‚ª•K—v‚Èˆê•”‚Ìƒ‰ƒCƒuƒ‰ƒŠ‚ğƒrƒ‹ƒh‚µ‚Ü‚·
 rem jpeglib‚ğƒrƒ‹ƒh
 echo ===========================jpegsr9c‚Ìƒrƒ‹ƒhŠJn===========================
 cd ./jpegsr9c/jpeg-9c
-call "%VSDEVCMD%"
-SET Include=%Include%;%INCLUDE_DIR%
+call %VSDEVCMD%
+SET Include=!Include!;!INCLUDE_DIR!
 copy jconfig.vc jconfig.h
 nmake /f makefile.vc clean all
 cd ../../
@@ -105,3 +107,5 @@ echo https://www.microsoft.com/en-us/download/details.aspx?id=8279
 echo ‰½‚©ƒL[‚ğ‰Ÿ‚·‚ÆI—¹‚µ‚Ü‚·[37m
 pause
 goto :EOF
+
+ENDLOCAL
